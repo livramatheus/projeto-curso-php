8fef539f9fe37347f30dcc615bd085c736e5faf7
livramateus <livra.matheus@gmail.com>
2022-04-25 21:05:31 -0300

    Minor changes

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$p   = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_STRING);
-$dir = './pages/' .$p. '.php';
+$page = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_STRING);
+$dir  = './pages/' .$page. '.php';
 
 if (file_exists($dir)) {
     require_once $dir;
